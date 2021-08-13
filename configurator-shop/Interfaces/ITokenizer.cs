0f374645a8f247e5674b40a2b007dc7806196c28
Lebedev Vladimir <goldswanru@gmail.com>
2021-08-13 12:56:42 +0300

    Добавлены сервисы авторизации и их интерфейсы

---
+++ configurator-shop/Interfaces/ITokenizer.cs
@@ -0,0 +1,8 @@
+﻿namespace configurator_shop.Interfaces
+{
+    public interface ITokenizer
+    {
+        public string GetRandomToken(); // Получить случайный токен
+        public string GetToken(string value); // Получить токен на основе значения
+    }
+}
---
+++ configurator-shop/Services/GmailSmtpEmailSender.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Mail;
+using configurator_shop.Interfaces;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MailKit.Net.Smtp;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace configurator_shop.Services
+{
+    public class GmailSmtpEmailSender : ISmtpEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public GmailSmtpEmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public EmailResult TryToSendMail(MailboxAddress to, string subject, MimeEntity body)
+        {
+            MimeMessage message = new MimeMessage();
+            
+            var from = new MailboxAddress("noreply", _configuration["SmtpConfiguration:SmtpUser"]);
+            
+            message.From.Add(from);
+            message.To.Add(to);
+            message.Subject = subject;
+            message.Body = body;
+
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                client.Connect(_configuration["SmtpConfiguration:SmtpServer"], 465, true);
+                client.Authenticate(_configuration["SmtpConfiguration:SmtpUser"], _configuration["SmtpConfiguration:SmtpPassword"]);
+                client.Send(message);
+                client.Disconnect(true);
+                client.Dispose();
+            }
+            catch
+            {
+                return EmailResult.SendFail;
+            }
+            
+            return EmailResult.SendSuccess;
+        }
+    }
+}
