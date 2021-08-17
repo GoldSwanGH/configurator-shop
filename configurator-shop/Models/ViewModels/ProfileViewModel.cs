a5e1ea08c4b7455f62508a82bbb3a7551dec9207
Lebedev Vladimir <goldswanru@gmail.com>
2021-08-17 14:58:19 +0300

    Профиль дополнен

---
+++ configurator-shop/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using configurator_shop.Models.EntityFrameworkModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace configurator_shop.Models.ViewModels
+{
+    public class ProfileViewModel : UserViewModel
+    {
+        [DisplayName("Изображение профиля")]
+        [BindProperty]
+        public IFormFile Image { get; set; }
+        
+        public static ProfileViewModel ToProfileViewModel(User user)
+        {
+            var model = new ProfileViewModel()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Tel = user.Tel,
+                CustomImage = user.CustomImage
+            };
+            return model;
+        }
+    }
+}
