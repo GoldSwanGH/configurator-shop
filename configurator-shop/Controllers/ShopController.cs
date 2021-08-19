f8da65770df9244384c8ddc8b68898b4cad42f02
Lebedev Vladimir <goldswanru@gmail.com>
2021-08-19 18:02:11 +0300

    Часть функционала добавления товаров для админа

--- configurator-shop/Controllers/ShopController.cs
+++ configurator-shop/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using configurator_shop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace configurator_shop.Controllers
@@ -24,6 +26,69 @@ namespace configurator_shop.Controllers
         
         public IActionResult Product()
         {
+            return View();
+        }
+
+        public IActionResult AddProductChooseCategory()
+        {
+            var model = new CategoriesViewModel();
+
+            model.Categories = new List<string>()
+            {
+                "CPU",
+                "GPU",
+                "Motherboard",
+                "Ram",
+                "Psu",
+                "Case",
+                "HDD",
+                "SDD",
+                "Cooler",
+                "Fan"
+            };
+            
+            return View(model);
+        }
+
+        public IActionResult AddProduct(string category)
+        {
+            switch (category)
+            {
+                case "CPU":
+                    
+                    break;
+                case "GPU":
+                    
+                    break;
+                case "Motherboard":
+                    
+                    break;
+                case "Ram":
+                    
+                    break;
+                case "Psu":
+                    
+                    break;
+                case "Case":
+                    
+                    break;
+                case "HDD":
+                    
+                    break;
+                case "SDD":
+                    
+                    break;
+                case "Cooler":
+                    
+                    break;
+                case "Fan":
+                    
+                    break;
+                default:
+                    
+                    break;
+            }
+
             return View();
         }
     }
